99d024b0d9462578ddab8ae9e7cac5d07c22dbb2
Andrey_Komar <andru_85@tut.by>
2023-04-16 19:04:53 +0300

    add BMI menu

--- BMI_3.py
+++ BMI_3.py
@@ -4,43 +4,54 @@ baza = {
       }
 
 def menu(): #print menu
-      print('\tМногопользовательский калькулятор БМИ: \nA.Вывести список пользователей \nВ.Посмотреть инфо о пользователе (рост, вес, шкала БМИ)\nC.Изменить данные о пользователе \nD.Удалить выбранного пользователя\nE.Добавить пользователя \nF.Выход')
+      print('\n\tМногопользовательский калькулятор БМИ: \nA.Вывести список пользователей \nВ.Посмотреть инфо о пользователе (рост, вес, шкала БМИ)\nC.Изменить данные о пользователе \nD.Удалить выбранного пользователя\nE.Добавить пользователя \nF.Выход')
 
-def repiat_add_user(user, v = 0, r= 0, p= 'men'): # reoiat and add user
+def change_add_user(user, v = 0, r= 0, p= 'men'): # change and add user
      baza[user] = {'ves' : v, 'rost': r, 'pol' : p}
-     print(baza.get(user))
+     print(f'{user}:', baza.get(user))
 
 def delete_user(user): # delete user
      del(baza[user])
      print(f'{user} удален') 
+
+def clear_term():
+     import os
+     os.system('CLS')
 
 
 menu() # begin 
 point = input('\tВыбери пункт меню -> ',)
 
 while point != 'F':
+      
       if point == "A": # выбор первого пункта меню - А
+          clear_term()
           print('Список пользователей:\n', baza.keys())
           menu()
           
       if point == "B":
+          clear_term()
           user = input('\tВыбери пользователя -> ',)
-          print(baza.get(user, 'Нет такого'))
+          while baza.get(user) == None:
+               print(f'{user} - Нет такого')
+               user = input('\tВыбери пользователя -> ',)
+          print(f'{user}:', baza.get(user))
           menu()
           
       if point == "C":
+           clear_term()
            user = input('\tВыбери пользователя для изменения-> ',)
            while baza.get(user) == None:
                print(f'{user} - Нет такого')
                user = input('\tВыбери пользователя для изменения-> ',)
-           print(baza.get(user))
            v = input('\tВес ?-> ',)
            r = input('\tРост ?-> ',)
            p = input('\tПол (men/women) ?-> ',)
-           repiat_add_user(user,v,r,p)
+           change_add_user(user,v,r,p)
            menu()
       
       if point == "D":
+           clear_term()
            user = input('\tВыбери пользователя для удаления -> ',)
            while baza.get(user) == None:
                print(f'{user} - Нет такого')
@@ -49,6 +60,7 @@ while point != 'F':
            menu()
 
       if point == "E":
+           clear_term()
            user = input('\tВыбери пользователя для добавления -> ',)
            while baza.get(user) != None:
                print(f'{user} - Такой уже есть')
@@ -56,8 +68,8 @@ while point != 'F':
            v = input('\tВес ?-> ',)
            r = input('\tРост ?-> ',)
            p = input('\tПол (men/women) ?-> ',)
-           repiat_add_user(user,v,r,p)
+           change_add_user(user,v,r,p)
            menu()
 
-      point = input('\tВыбери пункт меню -> ',)
+      point = input('\tВыбери пункт  меню -> ',)
      
